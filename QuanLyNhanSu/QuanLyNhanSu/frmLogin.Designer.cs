88adecdb6965d684f41c486e1bc4cea3f35b9ef6
SCHyperion <79067994+SCHyperion@users.noreply.github.com>
2021-02-18 14:56:19 +0700

    test check

--- QuanLyNhanSu/QuanLyNhanSu/frmLogin.Designer.cs
+++ QuanLyNhanSu/QuanLyNhanSu/frmLogin.Designer.cs
@@ -53,7 +53,7 @@ namespace QuanLyNhanSu
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(97, 166);
+            this.label2.Location = new System.Drawing.Point(81, 174);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 15);
             this.label2.TabIndex = 1;
@@ -63,7 +63,7 @@ namespace QuanLyNhanSu
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(97, 122);
+            this.label3.Location = new System.Drawing.Point(81, 130);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(90, 15);
             this.label3.TabIndex = 2;
@@ -71,23 +71,24 @@ namespace QuanLyNhanSu
             // 
             // txtTenTKhoan
             // 
-            this.txtTenTKhoan.Location = new System.Drawing.Point(193, 121);
+            this.txtTenTKhoan.Location = new System.Drawing.Point(177, 129);
             this.txtTenTKhoan.Name = "txtTenTKhoan";
             this.txtTenTKhoan.Size = new System.Drawing.Size(165, 20);
             this.txtTenTKhoan.TabIndex = 3;
             // 
             // txtMatKhau
             // 
-            this.txtMatKhau.Location = new System.Drawing.Point(193, 166);
+            this.txtMatKhau.Location = new System.Drawing.Point(177, 174);
             this.txtMatKhau.Name = "txtMatKhau";
             this.txtMatKhau.Size = new System.Drawing.Size(165, 20);
             this.txtMatKhau.TabIndex = 4;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(177, 220);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(162, 219);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(85, 31);
             this.button1.TabIndex = 5;
             this.button1.Text = "Đăng Nhập";
             this.button1.UseVisualStyleBackColor = true;
