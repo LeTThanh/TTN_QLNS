c332ea8bfdd56132ad2b1aea569b1c2493367c35
nguyenhuyhoanganh <huyhoanganh142000@gmail.com>
2021-05-12 14:56:19 +0700

    HA

--- QuanLyNhanSu/FormHopDong.cs
+++ QuanLyNhanSu/FormHopDong.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,90 @@ namespace QuanLyNhanSu
         public FormHopDong()
         {
             InitializeComponent();
+            load();
+        }
+        string ConnectionString = @"Data Source=LAPTOP-9PE5ABV1\SQLEXPRESS;Initial Catalog=QuanLyNhanSu1;Integrated Security=True";
+        DataTable ExecuteQuery(string query, object[] parameter = null)
+        {
+
+            DataTable data = new DataTable();
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+
+                connection.Open();
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                if (parameter != null)
+                {
+                    string[] temp = query.Split(' ');
+                    List<string> listPara = new List<string>();
+                    foreach (string item in temp)
+                    {
+                        if (item[0] == '@')
+                            listPara.Add(item);
+                    }
+                    for (int i = 0; i < parameter.Length; i++)
+                    {
+                        command.Parameters.AddWithValue(listPara[i], parameter[i]);
+                    }
+                }
+                try
+                {
+                    SqlDataAdapter adapter = new SqlDataAdapter(command);
+                    adapter.Fill(data);
+                }
+                catch { }
+
+                connection.Close();
+            }
+            return data;
+        }
+        // trả về 1 data table  gọi trong phương thức xem, lấy dữ liệu qua câu truy vấn
+        int ExecuteNonQuery(string query, object[] parameter = null)
+        {
+
+            int acceptedRows = 0;
+
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+
+                connection.Open();
+                try
+                {
+                    SqlCommand command = new SqlCommand(query, connection);
+
+                    if (parameter != null)
+                    {
+                        string[] temp = query.Split(' ');
+                        List<string> listPara = new List<string>();
+                        foreach (string item in temp)
+                        {
+                            if (item[0] == '@')
+                                listPara.Add(item);
+                        }
+                        for (int i = 0; i < parameter.Length; i++)
+                        {
+                            command.Parameters.AddWithValue(listPara[i], parameter[i]);
+                        }
+                    }
+                    //thực thi câu query chả về số dòng câu truy vấn thực hiện được
+                    acceptedRows = -1;
+
+                    acceptedRows = command.ExecuteNonQuery();
+                }
+                catch { MessageBox.Show("Lỗi Dữ Liệu"); }
+                connection.Close();
+            }
+
+            return acceptedRows;
+        }
+        // trả về kiểu int, gọi trong phương thức thêm và xoá, kiểu int là số dòng  thực thi thay dổi database
+        public void load()
+        {
+            string que = "Select * from HDLD";
+            guna2DataGridView1.DataSource = ExecuteQuery(que);
         }
     }
 }
